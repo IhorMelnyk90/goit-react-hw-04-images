b6e85f5a384ba40291568d60ab7beec331510874
Ihor Melnyk <91090917+IhorMelnyk90@users.noreply.github.com>
2022-10-11 09:48:21 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,8 @@
-import { useState, useEffect,} from 'react';
+import { useState, useEffect} from 'react';
 
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { animateScroll as scroll } from 'react-scroll';
+// import { animateScroll as scroll } from 'react-scroll';
 
 import Loader from 'components/Loader';
 import Api from 'components/fetchImages';
